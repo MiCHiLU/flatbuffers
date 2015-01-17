f037ec5def782f3ca54e0ff033fd3a145b31bc40
ENDOH takanao <djmchl@gmail.com>
2015-01-17 18:03:07 +0700

    do not import unsafe package when building on appengine

--- go/unsafe.go
+++ go/unsafe.go
@@ -1,3 +1,5 @@
+// +build !appengine
+
 package flatbuffers
 
 import "unsafe"
--- go/unsafe_appengine.go
+++ go/unsafe_appengine.go
@@ -1,32 +1,32 @@
-package flatbuffers
+// +build appengine
 
-import "unsafe"
+package flatbuffers
 
 var (
 	// See http://golang.org/ref/spec#Numeric_types
 
 	// SizeUint8 is the byte size of a uint8.
-	SizeUint8 = int(unsafe.Sizeof(uint8(0)))
+	SizeUint8 int = 1
 	// SizeUint16 is the byte size of a uint16.
-	SizeUint16 = int(unsafe.Sizeof(uint16(0)))
+	SizeUint16 int = 2
 	// SizeUint32 is the byte size of a uint32.
-	SizeUint32 = int(unsafe.Sizeof(uint32(0)))
+	SizeUint32 int = 4
 	// SizeUint64 is the byte size of a uint64.
-	SizeUint64 = int(unsafe.Sizeof(uint64(0)))
+	SizeUint64 int = 8
 
 	// SizeInt8  is the byte size of a int8.
-	SizeInt8 = int(unsafe.Sizeof(int8(0)))
+	SizeInt8 int = 1
 	// SizeInt16 is the byte size of a int16.
-	SizeInt16 = int(unsafe.Sizeof(int16(0)))
+	SizeInt16 int = 2
 	// SizeInt32 is the byte size of a int32.
-	SizeInt32 = int(unsafe.Sizeof(int32(0)))
+	SizeInt32 int = 4
 	// SizeInt64 is the byte size of a int64.
-	SizeInt64 = int(unsafe.Sizeof(int64(0)))
+	SizeInt64 int = 8
 
 	// SizeFloat32 is the byte size of a float32.
-	SizeFloat32 = int(unsafe.Sizeof(float32(0)))
+	SizeFloat32 int = 4
 	// SizeFloat64 is the byte size of a float64.
-	SizeFloat64 = int(unsafe.Sizeof(float64(0)))
+	SizeFloat64 int = 8
 
 	// SizeByte is the byte size of a byte.
 	// The `byte` type is aliased (by Go definition) to uint8.
@@ -37,9 +37,9 @@ var (
 	SizeBool = SizeUint8
 
 	// SizeSOffsetT is the byte size of an SOffsetT.
-	SizeSOffsetT = int(unsafe.Sizeof(SOffsetT(0)))
+	SizeSOffsetT int = 4
 	// SizeUOffsetT is the byte size of an UOffsetT.
-	SizeUOffsetT = int(unsafe.Sizeof(UOffsetT(0)))
+	SizeUOffsetT int = 4
 	// SizeVOffsetT is the byte size of an VOffsetT.
-	SizeVOffsetT = int(unsafe.Sizeof(VOffsetT(0)))
+	SizeVOffsetT int = 2
 )
